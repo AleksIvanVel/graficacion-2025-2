8a3c96a19c1035846c261781d7cc37187470abcf
salmachaveznavarro <salmachavez30@aragon.unam.mx>
2025-03-02 17:53:08 -0600

    video1_salma

---
+++ PONGV1/Assets/Scripts/Configuracion.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Configuracion : MonoBehaviour
+{
+    public Text op1, op2;
+    public static int tipoJuego = 1; // 1. compu, 2. persona
+
+
+    // Start is called before the first frame update
+    void Awake() {
+        tipoJuego = 1;
+        op1.gameObject.SetActive(true);
+        op2.gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.Alpha1)){
+            BorraSubrayado();
+            op1.gameObject.SetActive(true);
+            tipoJuego = 1;
+        }
+        if(Input.GetKey(KeyCode.Alpha2)){
+            BorraSubrayado();
+            op2.gameObject.SetActive(true);
+            tipoJuego = 2;
+        }
+        if(Input.GetKey(KeyCode.Space)){
+            SceneManager.LoadScene("Main");
+            
+        }
+
+    }
+    public void BorraSubrayado(){
+        op1.gameObject.SetActive(false);
+        op2.gameObject.SetActive(false);
+    }
+}
